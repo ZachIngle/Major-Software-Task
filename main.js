8f4ac9e85586ab8bb95fdd711d64b5ad2a953960
Zach <fzingle@internode.on.net>
2019-06-11 22:44:39 +1000

    Changed browser window to 1280x720. Adding sorting to table.

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 768,
+    width: 1280,
+    height: 720,
     resizable: false,
     webPreferences: {
       nodeIntegration: true
